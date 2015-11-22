a4227660a1bacdd455583dd009f1a70a32e38cd0
Steven Olson <steveoscaro@gmail.com>
2015-11-22 11:38:04 -0700

    I4: added changing background colors

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,17 +4,28 @@ require 'pry'
 
 number = rand(100)
 
+
 get '/' do
+
   guess = params["guess"].to_i
   message = check_guess(guess, number)
-  erb :index, :locals=> {:number => number, :message => message}
+  color = styling(message)
+  erb :index, :locals=> {:number => number, :message => message, :color => color}
 end
 
 def check_guess(guess, number)
-message =  "Too low" if guess < number && (number - guess) <= 5
-message =  "Way too low" if guess < number && (number - guess) > 5
-message =  "Too high" if guess > number && (guess - number) <= 5
-message =  "Way too high" if guess > number && (guess - number) > 5
-message =  "The SECRET NUMBER is #{number}" if guess ==  number
-message
+  message = "Too low" if guess < number && (number - guess) <= 5
+  message = "Way too low" if guess < number && (number - guess) > 5
+  message = "Too high" if guess > number && (guess - number) <= 5
+  message = "Way too high" if guess > number && (guess - number) > 5
+  message = "The SECRET NUMBER is #{number}" if guess ==  number
+  message = "Guess a number between 1 - 100" if guess == 0
+  message
+end
+
+def styling(message)
+  color = "OrangeRed" if message == "Way too low" || message == "Way too high"
+  color = "PeachPuff" if message == "Too low" || message == "Too high"
+  color = "Green" if message.include?("SECRET")
+  color
 end
